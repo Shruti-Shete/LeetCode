134c7a196dcccfee51a02d50834066e72621513d
Shruti Shete <66845616+Shruti-Shete@users.noreply.github.com>
2024-06-10 14:43:00 -0700

    Time: 909 ms (98.76%), Space: 0B (100.00%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -1,3 +1,5 @@
+# Write your MySQL query statement below
+
 Select product_id, year as first_year, quantity, price from Sales
-where (product_id, year) in
-(Select product_id, min(year) from Sales group by product_id)
+where (product_id, year) in (Select product_id, min(year) from Sales
+group by product_id )
