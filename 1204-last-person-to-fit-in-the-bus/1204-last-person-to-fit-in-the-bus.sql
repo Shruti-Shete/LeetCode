c287a9ec916a7aed87407bb38f2c27358e533250
Shruti Shete <66845616+Shruti-Shete@users.noreply.github.com>
2024-06-26 17:20:49 -0700

    Time: 1450 ms (59.24%), Space: 0B (100.00%) - LeetHub

--- 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -1,11 +1,12 @@
 # Write your MySQL query statement below
 
-With Temp as
-(Select turn, person_id, person_name, weight, sum(weight) over (order by turn) as Total 
-from queue)
-Select person_name from temp
-where Total <= 1000
-order by Total Desc limit 1
+SELECT 
+    q1.person_name
+FROM Queue q1 JOIN Queue q2 ON q1.turn >= q2.turn
+GROUP BY q1.turn
+HAVING SUM(q2.weight) <= 1000
+ORDER BY SUM(q2.weight) DESC
+LIMIT 1
 
 
 
