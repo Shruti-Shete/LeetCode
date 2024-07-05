bdde3545d68767e5d25c82216504ad5b6706e048
Shruti Shete <66845616+Shruti-Shete@users.noreply.github.com>
2024-07-04 21:46:28 -0700

    Time: 650 ms (63.46%), Space: 0B (100.00%) - LeetHub

--- 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -6,4 +6,4 @@ sum(case when state ="approved" then 1 else 0 end) as approved_count,
 sum(amount) as trans_total_amount, 
 sum(case when state = "approved" then amount else 0 end) as approved_total_amount
 from Transactions
-group by country, date_format(trans_date, "%y-%m")
+group by country, month
